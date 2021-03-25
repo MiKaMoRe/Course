702e0f296e157ba5ca168488adc0131f7c97598d
MiKa <kim.roman-2002@yandex.ru>
2021-03-25 15:08:57 +0300

    Changes
    15:08 25.03.2021

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,11 @@
+let $ = elem => document.querySelector(elem)
 
-const swiper = new Swiper('.slider__content.swiper-container', {
+let slider = new Swiper('.slider__content.swiper-container', {
   // Optional parameters
 	direction: 'horizontal',
 	loop: true,
 	slidesPerView: 1,
+	simulateTouch: false,
 	// If we need pagination
 	pagination: {
 		el: '.swiper-pagination',
@@ -14,7 +16,24 @@ const swiper = new Swiper('.slider__content.swiper-container', {
 	},
   // Navigation arrows
 	navigation: {
-		nextEl: '.button-next',
-		prevEl: '.button-prev',
+		nextEl: '.slider__button-next',
+		prevEl: '.slider__button-prev',
 	},
+});
+let ourClients = new Swiper('.our-clients__slider.swiper-container', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	// If we need pagination
+	autoplay: {
+		delay: 5000
+	},
+  	// Navigation arrows
+	navigation: {
+		nextEl: '.our-clients__button-next',
+		prevEl: '.our-clients__button-prev',
+	},
+	slidesPerView: 5,
+	spaceBetween: 20,
+	grabCursor: true,
 });
